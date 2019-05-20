d797232ad9c7b6f9b802a8c51cc4d939b5b109a7
Zhang Chao <35788176+hitzhangchao@users.noreply.github.com>
2019-05-20 21:45:00 +0800

    Update Angle_pwm_test.ino

--- Test/Angle_pwm_test.ino
+++ Test/Angle_pwm_test.ino
@@ -26,6 +26,60 @@ float K1 = 0.05, Q_angle = 0.001, Q_gyro = 0.005, R_angle = 0.5, C_0 = 1;
 float dt = 0.005;			//fliter sampping time 5ms
 int addr = 0;
 
+float Angle;
+
+/*************************************************************
+Function: calculate the PWM from angle of  twist
+    angle     PWM
+   -20 ~ +20      -250 ~ 250
+**************************************************************/
+int angle_2_pwm(float angle)
+{
+  //limit the amplitude of angle value
+  int max_angle = 20;
+  if (angle > max_angle)  angle = max_angle;
+  if (angle < -max_angle) angle = -max_angle;
+
+  static int tmp_angle;
+  tmp_angle = 12.5*angle;   //12.5 comes from K = Max_PWM/Max_angle = 250/20 = 12.5
+  return tmp_angle;
+}
+
+/*************************************************************
+Function: set PWM value to motor driver
+      0~250 reprented for duty cycle of 0~100%
+**************************************************************/
+void set_PWM(int motor_PWM_L)
+{
+  //limit the amplitude of PWM value, or use map()
+  int max = 250;
+  if (motor_PWM_L > max)  motor_PWM_L = max;
+  if (motor_PWM_L < -max) motor_PWM_L = -max;
+
+  //write PWM value to motor driver board
+  if (motor_PWM_L > 0)
+  {
+    digitalWrite(IN1_L, HIGH);
+    digitalWrite(IN2_L, LOW);
+  }
+  else
+  {
+    digitalWrite(IN1_L, LOW);
+    digitalWrite(IN2_L, HIGH);
+  }
+  analogWrite(PWM_L, abs(motor_PWM_L));
+}
+
+void control()
+{
+	sei();
+	IMU.getMotion6(&ax, &ay, &az, &gx, &gy, &gz);
+	KF.Angletest(ax, ay, az, gx, gy, gz, dt, Q_angle, Q_gyro, R_angle, C_0, K1);
+	Angle = KF.angle;
+	Angle_PWM = angle_2_pwm(Angle);
+	set_PWM(Angle_PWM);
+}
+
 void setup()
 {
 	//pins about motor drivers
@@ -46,58 +100,8 @@ void setup()
 }
 
 void loop()
-{}
-
-void control()
 {
-	sei();
-	IMU.getMotion6(&ax, &ay, &az, &gx, &gy, &gz);
-	KF.Angletest(ax, ay, az, gx, gy, gz, dt, Q_angle, Q_gyro, R_angle, C_0, K1);
-	Serial.print("Angle: "); Serial.print(KF.angle); Serial.print("  ");
-
-	Angle_PWM = angle_2_pwm(KF.angle);
-	set_PWM(Angle_PWM);
-	Serial.print("PWM: "); Serial.print(Angle_PWM); Serial.println();
-}
-
-/*************************************************************
-Function: calculate the PWM from angle of  twist
-		angle			PWM
-	 -20 ~ +20      -250 ~ 250
-**************************************************************/
-int angle_2_pwm(float angle)
-{
-	//limit the amplitude of angle value
-	int max_angle = 20;
-	if (angle > max_angle)	angle = max_angle;
-	if (angle < -max_angle)	angle = -max_angle;
-
-	static int tmp_angle;
-	tmp_angle = 12.5*angle;		//12.5 comes from K = Max_PWM/Max_angle = 250/20 = 12.5
-	return tmp_angle;
-}
-
-/*************************************************************
-Function: set PWM value to motor driver
-		  0~250 reprented for duty cycle of 0~100%
-**************************************************************/
-void set_PWM(int motor_PWM_L)
-{
-	//limit the amplitude of PWM value, or use map()
-	int max = 250;
-	if (motor_PWM_L > max)	motor_PWM_L = max;
-	if (motor_PWM_L < -max)	motor_PWM_L = -max;
-
-	//write PWM value to motor driver board
-	if (motor_PWM_L > 0)
-	{
-		digitalWrite(IN1_L, HIGH);
-		digitalWrite(IN2_L, LOW);
-	}
-	else
-	{
-		digitalWrite(IN1_L, LOW);
-		digitalWrite(IN2_L, HIGH);
-	}
-	analogWrite(PWM_L, abs(motor_PWM_L));
+	//Serial.print("Angle: "); Serial.print(Angle); 
+	//Serial.print("  PWM: "); Serial.print(Angle_PWM); 
+	//Serial.println();
 }
